b47439cc5ab094288d608539333d837d1ce2a826
grynka <ihor.kozhemyakin@gmail.com>
2022-12-01 17:39:15 +0200

    Update feedback.js

--- src/components/feedback/feedback.js
+++ src/components/feedback/feedback.js
@@ -29,13 +29,16 @@ class Feedback extends React.Component {
     }
 
     countTotalFeedback = () => {
-         return (this.state.bad + this.state.neutra + this.state.good)
-
+         return (
+            <p>Total: {this.state.good + this.state.neutral + this.state.bad}</p>)
+    
     }
 
     countPositiveFeedbackPercentage = () => {
-        console.log(this.countTotalFeedback()/this.state.bad)
-        return(this.countTotalFeedback()/this.state.bad)
+        console.log(this.state.bad)
+        return(<p>Positive feedback: {
+           this.state.good ? Math.round(((this.state.good/(this.state.good + this.state.neutral + this.state.bad))*100)) : 0 }
+            %</p>)
     } 
 
    render() {
@@ -47,14 +50,15 @@ class Feedback extends React.Component {
         <button type="button" onClick={this.neutralIncrement}>Neutral</button>
         <button type="button" onClick={this.badIncrement}>Bad</button>
         </div>
-        <p>Statistic</p>
+        <p>Statistic</p> 
+        { (this.state.good + this.state.neutral + this.state.bad) = 0 ?
         <div className="feedbackCounts">
             <p>Good: {this.state.good}</p>
             <p>Neutral: {this.state.neutral}</p>
             <p>Bad: {this.state.bad}</p>
-            <p>Total: {this.state.good + this.state.neutral + this.state.bad}</p>
-            <p>Positive feedback: render() {this.countTotalFeedback()}%</p>
-        </div>
+             {this.countTotalFeedback()}
+             {this.countPositiveFeedbackPercentage()}
+        </div> : <p>No feedback given</p> }
         </div>
     )
    }
